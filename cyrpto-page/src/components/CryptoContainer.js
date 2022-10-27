88808bd30be4920d84562dbd5b8b9408ec85e0fa
Diego Figueroa <diegofigueroa79@gmail.com>
2022-10-27 11:40:49 -0700

    add crypto container, list, and item component

---
+++ cyrpto-page/src/components/CryptoContainer.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { CryptoList } from "./CryptoList"
+
+export function CryptoContainer() {
+    return (
+        <div>
+            <CryptoList />
+        </div>
+    )
+}
---
+++ cyrpto-page/src/components/CryptoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function CryptoList() {
+    // grab c
+    //const cryptocurrencies = userSelector( state => state.cryptocurrencies )
+    return (
+        <ul>
+            { cryptocurrencies.map( cryptocurrency => {
+                
+            } )}
+        </ul>
+    )
+}
